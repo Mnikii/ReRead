c6faba1421a01392815938d56c6f461adc195c68
Olga <isaevaolya9@gmail.com>
2026-05-13 18:24:40 +0300

    fix: change structure

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from .user import User
+from .book import Book
+from .location import Location
+
+__all__ = ['User', 'Book', 'Location']
--- app/models/location.py
+++ app/models/location.py
@@ -1,4 +1,9 @@
+from ..extensions import db
+
+
 class Location(db.Model):
+    __tablename__ = 'locations'
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(200), nullable=False)
