5f881485030816d05cbfcfcdb5a20837dbea502f
Olga <isaevaolya9@gmail.com>
2026-05-13 17:30:13 +0300

    feat: add Location model

---
+++ models.py
@@ -0,0 +1,4 @@
+class Location(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
