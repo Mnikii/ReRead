55de0cdb83908ffbce21000371a94884fa2405cd
Olga <isaevaolya9@gmail.com>
2026-05-13 17:33:46 +0300

    feat: add books search

---
+++ routes/search.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request, jsonify
+from models import Book, Location
+
+search_bp = Blueprint('search', __name__)
+
+# GET /books список всех книг с фильтрами
+@search_bp.route('/books', methods=['GET'])
+def get_books():
+    # Берём параметры из строки запроса
+    # например /books?title=гарри&author=роулинг
+    title = request.args.get('title')
+    author = request.args.get('author')
+    city = request.args.get('city')
+
+    query = Book.query
+
+    # Применяем запрошенные фильтры
+    if title:
+        query = query.filter(Book.title.like(f'%{title}%'))
+    if author:
+        query = query.filter(Book.author.like(f'%{author}%'))
+    if city:
+        query = query.filter(Book.city.like(f'%{city}%'))
+
+    books = query.all()
+
+    # Делаем из списка объектов в список словарей
+    result = []
+    for book in books:
+        result.append({
+            'id': book.id,
+            'title': book.title,
+            'author': book.author,
+            'condition': book.condition,
+            'is_reserved': book.is_reserved,
+            'owner_id': book.owner_id
+        })
+
+    return jsonify(result)
